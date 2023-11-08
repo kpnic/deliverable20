a4f1c8b877b762f3e940e59489c4dc50ff22ed0a
kpnic <144055190+kpnic@users.noreply.github.com>
2023-11-07 21:13:16 -0500

    Add files via upload

--- js/video.js
+++ js/video.js
@@ -1,28 +1,69 @@
-var video = document.querySelector("video");
+let videoControl;
 
 window.addEventListener("load", function() {
-	console.log("Good job opening the window")
-    video.autoplay = false;
-    video.loop = false;
+	console.log("Good job opening the window");
+    videoControl = document.getElementById("player1");
+    videoControl.autoplay = false;
+    videoControl.loop = false;
 });
 
-document.querySelector("#play").addEventListener("click", function() {
+function updateVolume() {
+	const volumeDisplay = document.getElementById("volume");
+	volumeDisplay.innerText = `${Math.round(videoControl.volume * 100)}%`;
+}
+
+document.getElementById("play").addEventListener("click", function() {
 	console.log("Play Video");
-    video.play();
+    videoControl.play();
+    updateVolume();
 });
 
-document.querySelector("#pause").addEventListener("click", function () {
+document.getElementById("pause").addEventListener("click", function () {
     console.log("Pause Video");
-    video.pause();
+    videoControl.pause();
 });
 
-document.querySelector("#menu").addEventListener("click", function () {
-    console.log("Video Muted");
-    video.muted = true;
-    slider.min = true;
+document.getElementById("slower").addEventListener("click", function() {
+	console.log("Slow Video");
+	videoControl.playbackRate -= 0.1;
+    console.log(`New speed: ${videoControl.playbackRate}`);
 });
 
-document.querySelector("#vintage").addEventListener("click", function () {
-    console.log("Old School!");
-    document.querySelector("#vintage").innerHTML;
+document.getElementById("faster").addEventListener("click", function() {
+	console.log("Speed Up Video");
+    videoControl.playbackRate += (videoControl.playbackRate * 0.1);
+    console.log(`New speed: ${videoControl.playbackRate}`);
+});
+
+document.getElementById("skip").addEventListener("click", function() {
+    videoControl.currentTime += 10;
+    if (videoControl.currentTime > videoControl.duration) {
+        videoControl.currentTime = 0;
+    }
+    console.log(`Current location: ${videoPlayer.currentTime}`);
+  });
+
+  mute = document.getElementById("mute")
+  mute.addEventListener("click", function() {
+    if (videoControl.muted) {
+        videoControl.muted = false;
+      mute.innerText = "Mute";
+    } else {
+      videoControl.muted = true;
+      mute.innerText = "Unmute";
+    }
+  });
+
+  slider = document.getElementById("slider")
+  slider.addEventListener("input", function() {
+    videoControl.volume = slider.value / 100;
+    updateVolume();
+  });
+  
+document.getElementById("vintage").addEventListener("click", function () {
+    videoControl.classList.add('oldSchool');
+});
+
+document.getElementById("orig").addEventListener("click", function() {
+    videoControl.classList.remove('oldSchool');
 });
